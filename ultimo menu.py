3357af5cfe02e6bebc967936ff40ec55978be387
Jsebastian26 <sebastian00614@gmail.com>
2026-03-29 21:53:50 -0500

    Add files via upload

---
+++ ultimo menu.py
@@ -0,0 +1,28 @@
+while True:
+    print("\n--- CRUD ESTUDIANTES ---")
+    print("1. Agregar")
+    print("2. Mostrar")
+    print("3. Actualizar")
+    print("4. Eliminar")
+    print("5. Salir")
+
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        ()
+
+    elif opcion == "2":
+        ()
+
+    elif opcion == "3":
+        ()
+
+    elif opcion == "4":
+        ()
+
+    elif opcion == "5":
+        print(" Saliendo del programa...")
+        break  
+
+    else:
+        print(" Opción inválida")
